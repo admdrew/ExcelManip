cf62279a4a6930fd6ca0c417832ff3f7bd042a0e
admdrew <admdrew@gmail.com>
2014-02-14 10:48:22 -0600

    excel class
    - added excel class from codeproject.com
    - creates document and tabs, outputs duplicated 'subject' names
    
    - still needs rest test row information to be populated
    - putting all test rows in first tab, rest of tabs are created but empty

--- Program.cs
+++ Program.cs
@@ -48,15 +48,16 @@ namespace ExcelManip {
          * Author: ageorge
          * 
          * in: (INPUT_FILENAME[|OUTPUT_FILENAME])
-         * - creates a spreadsheet (OUTPUT_FILENAME) with an expanded shell of ALM test cases from INPUT_FILENAME
+         * - creates a doc (OUTPUT_FILENAME) with an expanded shell of ALM test cases from INPUT_FILENAME
          */
         static void Main(string[] args) {
             string INPUT_FILENAME = string.Empty;
+            INPUT_FILENAME = "ius8-summary.xlsx"; // breakpoints/troubleshooting
             string OUTPUT_FILENAME = string.Empty;
             string strOF = string.Empty;
-            //string INPUT_FILENAME = "ius8-summary.xlsx"; // breakpoints/troubleshooting
             string PRODUCT = string.Empty;
             string OWNER = string.Empty;
+            int argumentLength;
 
             WorksheetPart wsFeatureList;
             OrderedDictionary odAreas = new OrderedDictionary();
@@ -65,10 +66,12 @@ namespace ExcelManip {
              * process arguments
              * INPUT_FILENAME|INPUT_FILENAME OUTPUT_FILENAME
              */
-            if (args.Length == 0) { // usage
+            argumentLength = args.Length;
+            if (string.IsNullOrEmpty(INPUT_FILENAME) && argumentLength == 0) { // usage
                 return;
             }
-            if (args.Length == 1 || args.Length == 2) {
+
+            if (argumentLength == 1 || argumentLength == 2) {
                 INPUT_FILENAME = args[0];
                 try {
                     OUTPUT_FILENAME = args[1];
@@ -81,13 +84,17 @@ namespace ExcelManip {
                     Console.Out.WriteLine("This only works with Excel 2007+ (.xlsx) spreadsheets.");
                     return;
                 }
-                else if (args.Length == 1) {
+                else if (argumentLength == 1) {
                     OUTPUT_FILENAME = INPUT_FILENAME.Replace(".xlsx", "-alm-import.xlsx");
                 }    
             }
+            else if (!string.IsNullOrEmpty(INPUT_FILENAME) && argumentLength == 0) { // debug?
+                OUTPUT_FILENAME = INPUT_FILENAME.Replace(".xlsx", "-alm-import.xlsx");
+            }
+
 
             /*
-             * read test case summary spreadsheet
+             * read test case summary doc
              * reads INPUT_FILENAME
              * - base on input filename? output filename configurable?
              */
@@ -150,7 +157,7 @@ namespace ExcelManip {
                      */
                     listFeatures.Add(new Feature() { FeatureName = featureName, NumberOfTests = numManTCs } );
                 } // end rows iteration
-            } // end reading spreadsheet
+            } // end reading doc
 
 
 
@@ -159,43 +166,61 @@ namespace ExcelManip {
             * build ALM import workbook
             * - base on input filename? output filename configurable?
             */
-            using (SpreadsheetDocument docOutput = SpreadsheetDocument.Create(OUTPUT_FILENAME, SpreadsheetDocumentType.Workbook)) {
-                SharedStringTablePart tablepart;
-                WorkbookStylesPart stylespart;
+            //using (SpreadsheetDocument docOutput = SpreadsheetDocument.Create(OUTPUT_FILENAME, SpreadsheetDocumentType.Workbook)) {
+            SpreadsheetDocument docOutput = Excel.CreateWorkbook(OUTPUT_FILENAME);
+            Worksheet worksheet;
+            //SharedStringTablePart tablepart;
+            //WorkbookStylesPart stylespart;
 
-                docOutput.AddWorkbookPart();
-                docOutput.WorkbookPart.Workbook = new Workbook();
+            //docOutput.AddWorkbookPart();
+            //docOutput.WorkbookPart.Workbook = new Workbook();
 
-                tablepart = docOutput.WorkbookPart.AddNewPart<SharedStringTablePart>();
-                tablepart.SharedStringTable = new SharedStringTable();
-                    
+            //tablepart = docOutput.WorkbookPart.AddNewPart<SharedStringTablePart>();
+            //tablepart.SharedStringTable = new SharedStringTable();
 
-                // iterate through all areas
-                IDictionaryEnumerator enumAreas = odAreas.GetEnumerator();
-                while (enumAreas.MoveNext()) {
-                    List<Feature> outputFeatures = (List<Feature>)enumAreas.Value;
+            // iterate through all areas
+            IDictionaryEnumerator enumAreas = odAreas.GetEnumerator();
+            uint outputRow = 1;
+            int numFeatures = -1;
+            int numTests = -1;
 
-                    // create worksheet for current area
+            while (enumAreas.MoveNext()) {
+                List<Feature> outputFeatures = (List<Feature>)enumAreas.Value;
+                string area = enumAreas.Key.ToString();
 
+                // create worksheet for current area
+                Excel.AddWorksheet(docOutput, area);
+                worksheet = docOutput.WorkbookPart.WorksheetParts.First().Worksheet;
 
-                    // iterate through all features
-                    foreach (Feature aFeature in outputFeatures) {
-                        Console.Out.WriteLine(enumAreas.Key.ToString() + " " + aFeature);
+                // iterate through all features
+                numFeatures = outputFeatures.Count;
+                foreach (Feature aFeature in outputFeatures) {
+                    Console.Out.WriteLine(area + " " + aFeature);
 
-                        // get number of tests
-                        // ensure we have rest of TC data (product/status/owner/etc)
-
-                        // add rows (current feature, duplicated over number of tests)
+                    // get number of tests
+                    numTests = aFeature.NumberOfTests;
+                    // ensure we have rest of TC data (product/status/owner/etc)
+                        
+                    // add rows (current feature, duplicated over number of tests)
+                    for (int i = 0; i < numTests; i++) {
+                        Excel.SetCellValue(docOutput, worksheet, 1, outputRow, aFeature.FeatureName, true);
+                        outputRow++;
                     }
-                } // end iterating through areas, data should be in spreadsheet
+                }
 
-                // styling
+                worksheet.Save();
+            } // end iterating through areas, data should be in doc
 
-                // save spreadsheet
+            docOutput.Close();
+            System.Diagnostics.Process.Start(OUTPUT_FILENAME);
 
-                // exit
-                return;
-            } // end creating spreadsheet
+            // styling
+
+            // save doc
+
+            // exit
+                //return;
+            //} // end creating doc
         } // end Main(args)
 
         /*
@@ -250,5 +275,8 @@ namespace ExcelManip {
 
             return value;
         }
+
+
+
     }
 }
